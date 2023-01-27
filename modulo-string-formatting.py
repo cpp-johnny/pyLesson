124e9fcf2037e9e4e5a5deaf558733a3f1e0be47
Johnny <119715263+cpp-johnny@users.noreply.github.com>
2023-01-27 13:32:03 +0800

    Create modulo-string-formatting.py

---
+++ modulo-string-formatting.py
@@ -0,0 +1,18 @@
+print("%d %s cost $%.2f" % (6, "bananas", 1.74))
+# output: 6 bananas cost $1.74
+
+
+"""
+
+<type>	  Conversion Type
+d, i, u	  Decimal integer
+x, X	    Hexadecimal integer
+o	Octal   integer
+f, F	    Floating-point
+e, E	E   notation
+g, G	    Floating-point or E notation
+c	        Single character
+s, r, a	  String
+%	        Single '%' character
+
+"""
